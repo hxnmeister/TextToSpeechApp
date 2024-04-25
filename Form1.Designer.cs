43b649558d50cdba56241d0daf28a32743b339a1
Valerii <valeriy.stronskiy@gmail.com>
2024-04-25 11:57:01 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace TextToSpeechApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.InputTextBox = new System.Windows.Forms.TextBox();
+            this.SendRequestButton = new System.Windows.Forms.Button();
+            this.LanguagesComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // InputTextBox
+            // 
+            this.InputTextBox.Location = new System.Drawing.Point(12, 12);
+            this.InputTextBox.Multiline = true;
+            this.InputTextBox.Name = "InputTextBox";
+            this.InputTextBox.Size = new System.Drawing.Size(556, 217);
+            this.InputTextBox.TabIndex = 0;
+            // 
+            // SendRequestButton
+            // 
+            this.SendRequestButton.Location = new System.Drawing.Point(466, 235);
+            this.SendRequestButton.Name = "SendRequestButton";
+            this.SendRequestButton.Size = new System.Drawing.Size(102, 37);
+            this.SendRequestButton.TabIndex = 1;
+            this.SendRequestButton.Text = "Convert Text To Speech";
+            this.SendRequestButton.UseVisualStyleBackColor = true;
+            this.SendRequestButton.Click += new System.EventHandler(this.SendRequestButton_Click);
+            // 
+            // LanguagesComboBox
+            // 
+            this.LanguagesComboBox.FormattingEnabled = true;
+            this.LanguagesComboBox.Location = new System.Drawing.Point(12, 235);
+            this.LanguagesComboBox.Name = "LanguagesComboBox";
+            this.LanguagesComboBox.Size = new System.Drawing.Size(121, 21);
+            this.LanguagesComboBox.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(580, 284);
+            this.Controls.Add(this.LanguagesComboBox);
+            this.Controls.Add(this.SendRequestButton);
+            this.Controls.Add(this.InputTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Text To Speech";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox InputTextBox;
+        private System.Windows.Forms.Button SendRequestButton;
+        private System.Windows.Forms.ComboBox LanguagesComboBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.CognitiveServices.Speech;
+using System;
+using System.Windows.Forms;
+
+namespace TextToSpeechApp
+{
+    public partial class Form1 : Form
+    {
+        private const string API_KEY = "4c371481854744209c6063ddcbab1ad6";
+        private const string API_REGION = "eastus";
+        private const string AVAILABLE_LANGUAGES = "English,Deutsch,Ukrainian";
+        public Form1()
+        {
+            InitializeComponent();
+
+            LanguagesComboBox.Items.AddRange(AVAILABLE_LANGUAGES.Split(','));
+        }
+
+        private async void SendRequestButton_Click(object sender, EventArgs e)
+        {
+            string convertingText = InputTextBox.Text;
+
+            try
+            {
+                if (string.IsNullOrEmpty(convertingText))
+                {
+                    throw new ArgumentNullException("Text for converting is null or empty!");
+                }
+                else if (LanguagesComboBox.SelectedItem == null)
+                {
+                    throw new Exception("Language is not selected!");
+                }
+
+                SpeechConfig config = SpeechConfig.FromSubscription(API_KEY, API_REGION);
+
+                switch(LanguagesComboBox.SelectedItem.ToString())
+                {
+                    case "English":
+                        config.SpeechSynthesisVoiceName = "en-GB-RyanNeural";
+                        break;
+                    case "Ukrainian":
+                        config.SpeechSynthesisVoiceName = "uk-UA-PolinaNeural";
+                        break;
+                    case "Deutsch":
+                        config.SpeechSynthesisVoiceName = "de-DE-ConradNeural";
+                        break;
+                }
+
+                using (SpeechSynthesizer synth = new SpeechSynthesizer(config))
+                {
+                    using (SpeechSynthesisResult result = await synth.SpeakTextAsync(convertingText))
+                    {
+                        OutputSpeechSynthesisResult(result, convertingText);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        static private void OutputSpeechSynthesisResult(SpeechSynthesisResult result, string text)
+        {
+            switch (result.Reason)
+            {
+                case ResultReason.SynthesizingAudioCompleted:
+                    Console.WriteLine($"Speech synthesized for text: [{text}]");
+                    break;
+                case ResultReason.Canceled:
+                    SpeechSynthesisCancellationDetails cancellation = SpeechSynthesisCancellationDetails.FromResult(result);
+                    MessageBox.Show($"CANCELED: Reason={cancellation.Reason}", "Canceled", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                    if (cancellation.Reason == CancellationReason.Error)
+                    {
+                        MessageBox.Show($"CANCELED:\nErrorCode={cancellation.ErrorCode}\nErrorDetails=[{cancellation.ErrorDetails}]\nDid you set the speech resource key and region values?", 
+                            "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
